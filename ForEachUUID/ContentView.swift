995cbb3dd951ec211e6f2ef5aa5b06a1d9214cdd
Francisco Casillas <chiccasillas@gmail.com>
2025-02-24 13:57:03 -0600

    Create dynamic lists and display them using UUID and not using it.

--- ForEachUUID/ContentView.swift
+++ ForEachUUID/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let users = ["Max", "Anna", "Chris"]
+    
+    struct Product {
+        var id: Int
+        var productName: String
+        var productPrice: Double
+    }
+    
+    let products = [
+        Product(id: 001, productName: "Box", productPrice: 22),
+        Product(id: 002, productName: "Chopstick", productPrice: 30),
+        Product(id: 003, productName: "Bottle", productPrice: 15)
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(users, id: \.self) { user in
+                Text(user)
+            }
+            Divider()
+            ForEach(products, id: \.id) { product in 
+                VStack {
+                    Text(product.productName)
+                    Text(String(format: "%.2f", product.productPrice))
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
