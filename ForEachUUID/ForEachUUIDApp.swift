774889687cfc760abf14df660caf1b092f8a5b2f
Francisco Casillas <chiccasillas@gmail.com>
2025-02-24 13:44:11 -0600

    Initial Commit

---
+++ ForEachUUID/ForEachUUIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachUUIDApp.swift
+//  ForEachUUID
+//
+//  Created by Francisco Jean on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ForEachUUIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
